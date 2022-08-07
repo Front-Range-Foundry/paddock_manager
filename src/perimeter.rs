6c849c69b5d05c7cfa4489f853d10e6b637bbe71
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-07 11:23:10 -0600

    Copilot is better at math than I am.
    - Wrote the area tests
    - Wrote the perimeter tests
    - All pass muster.

--- src/perimeter.rs
+++ src/perimeter.rs
@@ -24,6 +24,18 @@ impl Perimeter {
       area = area.abs() / 2.0;
       area
   }
+
+  fn perimeter(&self) -> f64 {
+	let mut perimeter = 0.0;
+	let mut i = 0;
+	let mut j = self.vertices.len() - 1;
+	while i < self.vertices.len() {
+	  perimeter += self.vertices[i].distance_from(&self.vertices[j]);
+	  j = i;
+	  i += 1;
+	}
+	perimeter
+  }
 }
 
 #[cfg(test)]
@@ -96,5 +108,31 @@ mod tests {
     ];
     let perimeter = Perimeter::new(vertices);
     assert_eq!(perimeter.area(), 12500.0);
+  }
+
+  #[test]
+  fn it_can_calculate_its_perimeter() {
+	let vertices = vec![
+	  Vertex::new(100.0, 100.0),
+	  Vertex::new(200.0, 100.0),
+	  Vertex::new(200.0, 200.0),
+	  Vertex::new(100.0, 200.0),
+	];
+	let perimeter = Perimeter::new(vertices);
+	assert_eq!(perimeter.perimeter(), 400.0);
+  }
+
+  #[test]
+  fn it_can_calculate_its_perimeter_as_a_polygon() {
+	let vertices = vec![
+	  Vertex::new(100.0, 100.0),
+	  Vertex::new(200.0, 100.0),
+	  Vertex::new(200.0, 200.0),
+	  Vertex::new(150.0, 250.0),
+	  Vertex::new(100.0, 200.0),
+    ];
+
+	let perimeter = Perimeter::new(vertices);
+	assert_eq!(perimeter.perimeter(), 441.4213562373095);
   }
 }
